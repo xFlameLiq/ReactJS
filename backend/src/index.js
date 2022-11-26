acf7ac7be0d631c9965c76ed1dd1146ce33530b7
xFlameLiq <benjamincore2002@hotmail.com>
2022-11-26 15:56:14 -0600

    Backend: Registro terminado, verificacion de login semiterminado

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,35 +1,46 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-const connection = require('./db');
-
-
 const app = express();
 
+const credentials = require('./db');
 
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(cors());
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello World' });
+  console.log("Hola mundo");
 });
 
 app.post('/login', (req, res) => {
-  res.json({ message: 'Login' });
+  const {email, password} = req.body;
+  console.log(email, password);
+  credentials.query(`SELECT * FROM users WHERE email = "${email}" AND password = "${password}"`, (err, result) => {
+    if(err) {
+      res.json({ err }); 
+    } else {
+      res.json({ result});
+    }
+  });
 })
 
 app.post('/register', (req, res) => {
   const {name, lastname, email, password, sex, phone} = req.body;
-  connection.query(`INSERT INTO users (name, lastname, email, password, sex, phone) VALUES ('${name}', '${lastname}', '${email}', '${password}', '${sex}', '${phone}')`, (err, result) => {
-    if (err) res.json({ message: 'Error' });
-    res.json({ message: 'User created' });
+  credentials.query(`INSERT INTO users (name, lastname, email, password, sex, phone) VALUES ("${name}", "${lastname}", "${email}", "${password}", "${sex}", "${phone}")`, (err, result) => {
+    if (err)  {
+      res.json({ message: 'Error' }); 
+    } else {
+      res.json({ message: 'User created' });
+    }
   });
 });
 
 
 
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
-});
