f2a27e2250284e3786fa2b78510fa8ea3590bceb
Armando Salazar <armandosalazar351@gmail.com>
2022-11-25 20:29:47 -0600

    register form works

--- reactjs/src/pages/Home.jsx
+++ reactjs/src/pages/Home.jsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
+      <Link to='/register'>Register</Link>
+      <br />
+      <Link to='/login'>Login</Link>
     </div>
   );
 }
