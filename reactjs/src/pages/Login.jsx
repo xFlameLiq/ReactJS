94d062d1e812a1531dbb3596933fd07463604a12
Armando Salazar <armandosalazar351@gmail.com>
2022-11-25 18:50:53 -0600

    components

---
+++ reactjs/src/pages/Login.jsx
@@ -0,0 +1,15 @@
+export default function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <input type="text" name="" id="" />
+        <input type="text" name="" id="" />
+        <input type="text" name="" id="" />
+        <input type="text" name="" id="" />
+        <input type="password" name="" id="" />
+        <input type="submit" value="enviar" />
+      </form>
+    </div>
+  );
+}
