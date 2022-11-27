63b6a9dc179f81b60aa95c994a941a833c1c58c5
Nahomy Hernandez <nahomyhernandez751@gmail.com>
2022-11-26 23:08:23 -0600

    Frontend login

--- reactjs/src/pages/Login.jsx
+++ reactjs/src/pages/Login.jsx
@@ -1,15 +1,20 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import './Login.css';
+
 export default function Login() {
 
   return (
-    <div>
+    <div className="container-main">
+      <form className="form">
       <h1>Login</h1>
-      <form>
-        <input type="text" name="" id="" />
-        <input type="text" name="" id="" />
-        <input type="text" name="" id="" />
-        <input type="text" name="" id="" />
-        <input type="password" name="" id="" />
-        <input type="submit" value="enviar" />
+      <label>Correo:</label>
+        <input className="input" type="text" name="" id="" />
+        <label>Contraseña:</label>
+        <input className="input" type="password" name="" id="" />
+        <input className="input" type="submit" value="enviar" />
+        <Link className="a" to='/Register'>¿No tienes una cuenta? Register</Link>
+          {/* <Link to='/'>Home</Link> */}
       </form>
     </div>
   );
