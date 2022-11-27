b42ced66c6f252194e5a59c726136219ffd0a442
xFlameLiq <benjamincore2002@hotmail.com>
2022-11-26 23:49:34 -0600

    Frontend listo para trabajar

--- reactjs/src/pages/Login.jsx
+++ reactjs/src/pages/Login.jsx
@@ -7,11 +7,21 @@ export default function Login() {
   return (
     <div className="container-main">
       <form className="form">
-      <h1>Login</h1>
-      <label>Correo:</label>
-        <input className="input" type="text" name="" id="" />
+      
+      <div className="sesion-img">
+        
+      </div>
+      <h1 className="title">Login</h1>
+          <div className="data">
+          <label>Correo:</label>
+        < input className="input" type="text" name="" id="" />
+          </div>
+       
+        <div className="data">
         <label>Contraseña:</label>
         <input className="input" type="password" name="" id="" />
+        </div>
+        
         <input className="input" type="submit" value="enviar" />
         <Link className="a" to='/Register'>¿No tienes una cuenta? Register</Link>
           {/* <Link to='/'>Home</Link> */}
